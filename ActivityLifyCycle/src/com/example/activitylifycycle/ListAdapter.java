b56d7a6ff312195bbe0a9de4767f18e4bbb9197e
zhangbi <dumuqiao0317@gmail.com>
2014-06-18 19:23:12 +0800

    adapter

---
+++ ActivityLifyCycle/src/com/example/activitylifycycle/ListAdapter.java
@@ -0,0 +1,39 @@
+package com.example.activitylifycycle;
+
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.SimpleAdapter;
+
+public class ListAdapter extends BaseAdapter {
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
