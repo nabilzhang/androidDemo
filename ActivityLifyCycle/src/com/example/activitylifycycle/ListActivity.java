d0e4cab65c2a1dbdcbf86e3fa582bfb172271dbb
zhangbi <dumuqiao0317@gmail.com>
2014-06-19 10:17:43 +0800

    listview

--- ActivityLifyCycle/src/com/example/activitylifycycle/ListActivity.java
+++ ActivityLifyCycle/src/com/example/activitylifycycle/ListActivity.java
@@ -1,24 +1,43 @@
 package com.example.activitylifycycle;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListActivity extends Activity {
-	
+
 	private ListView listView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
-		listView =(ListView)findViewById(R.id.list);
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
-		        android.R.layout.simple_list_item_1, new String[]{"a","b"});
+		listView = (ListView) findViewById(R.id.list);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, new String[] { "a", "b" });
 		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@SuppressLint("ShowToast")
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Toast.makeText(
+						ListActivity.this,
+						"text," + arg2 + "," + arg2 + ","
+								+ arg1.getClass().getCanonicalName() + ","
+								+ arg0.getClass().getCanonicalName(),
+						Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 	@Override
