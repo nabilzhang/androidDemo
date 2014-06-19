563401a853000224170c731991d8858466f2c234
zhangbi <dumuqiao0317@gmail.com>
2014-06-19 17:53:08 +0800

    9.png

--- ActivityLifyCycle/gen/com/example/activitylifycycle/R.java
+++ ActivityLifyCycle/gen/com/example/activitylifycycle/R.java
@@ -24,6 +24,7 @@ public final class R {
         public static final int a=0x7f020000;
         public static final int b=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int kfla=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
